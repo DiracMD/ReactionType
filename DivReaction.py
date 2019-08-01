254f5a31be02fe5d20192c2ec026da9708f73cee
Dirac-MD <29254355+junjinxiao@users.noreply.github.com>
2019-08-01 15:21:53 +0800

    enter

--- DivReaction.py
+++ DivReaction.py
@@ -1,22 +1,23 @@
 """ initial import the reference modules and library """
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd 
-
+import matplotlib
+import pandas as pd
 import os
 
-"""define the molecule to research"""
+"""define the molecule to research """
 molecule = "C2H4"
-reactantname = "C2H4" + "R.csv"                                                    
+reactantname = "C2H4" + "R.csv"                        
 # e.g.[H]C[H]=C([H])[H].
 os.mknod('/mnt/e/sub/decane/dodecane/3750/ff08/2400K/reactype/' + reactantname)
-productname = "C2H4" + "P.csv" 
-os.mknod('/mnt/e/sub/decane/dodecane/3750/ff08/2400K/reactype/' + productname)
+productname = "C2H4" + "P.csv"
 # e.g.[H]C[H]=C([H])[H].
+os.mknod('/mnt/e/sub/decane/dodecane/3750/ff08/2400K/reactype/' + productname)
+
 
 """ read the reaction file """
 with open("bonds.reaxc.reaction") as data:
-    new = [] 
+    new = []
     for line in data:
         nline = str(line.split()[0])
         sline = line.split()[1].split("->")
@@ -27,8 +28,6 @@ with open("bonds.reaxc.reaction") as data:
         f1 = open(reactantname, "a")
         f2 = open(productname, "a")
         if reaction[1] == "[H]C([H])=C([H])[H]":
-            print(reaction[0] + "," + reaction[1] + "," + reaction[2], file = f1)
+            print(reaction[0] + "," + reaction[1] + "," + reaction[2], file=f1)
         if reaction[2] == "[H]C([H])=C([H])[H]":
-            print(reaction[0] + "," + reaction[1] + "," + reaction[2], file = f2)
-
-    
+            print(reaction[0] + "," + reaction[1] + "," + reaction[2], file=f2)
--- F_BReactions.py
+++ F_BReactions.py
@@ -1,6 +1,25 @@
-#read the forword and back reaction data
-import numpy as np 
+""" initial import the reference modules and library """
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
 import pandas as pd
+import openbabel
+import rdkit 
+import os
 
-df = pd.read_csv("./C2H4P.csv")
-df.head()
+product = pd.read_csv("./C2H4P.csv", header=None)
+reactant = pd.read_csv("./C2H4R.csv", header=None)
+
+for j in range(len(reactant)):
+    for i in range(len(product)):
+        if reactant[2][j] == product[1][i]:
+            print(str(reactant[0][j])+","+str(reactant[2][j])+"->"+str(reactant[2][j])
+            +"   "+ str(product[0][i])+" "+str(product[1][i])+"->"+str(product[2][i]))
+
+#for x in range(0,20):
+    #z=df[0][x]
+    #y.append(z)
+"""
+plt.bar(x, height = y, width=0.8)
+plt.show()
+"""
